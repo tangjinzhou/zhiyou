361b4c614c98e573e2e35ca0059e054f1c55e731
tjz <415800467@qq.com>
2016-12-04 13:39:03 +0800

    初始化

--- App/containers/Start.js
+++ App/containers/Start.js
@@ -4,7 +4,7 @@ export default class Start extends React.Component {
     componentDidMount(){
         var myLatlng = new sogou.maps.LatLng(39.9952,116.3269);
         var map = new sogou.maps.Map(this.mapDom, {
-            zoom: 50,
+            zoom: 100,
             center: myLatlng,
             hdMap:true,
             mapTypeId: sogou.maps.MapTypeId.EDUSHIMAP,
@@ -20,7 +20,9 @@ export default class Start extends React.Component {
         return (
             <div className="start-container">
                 <div ref={(c)=>this.mapDom = c} className="map_canvas"></div>
-                <a href="#/map" className="btn">Go!</a>
+                <div className="btn-con">
+                    <a href="#/map" className="btn">Go!</a>
+                </div>
             </div>
         )
     }
