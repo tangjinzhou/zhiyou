a5c9250315d41b96434c8e8435dc200bd86b7990
tjz <415800467@qq.com>
2016-12-04 13:33:38 +0800

    初始化

--- App/containers/Start.js
+++ App/containers/Start.js
@@ -1,11 +1,26 @@
 import React from 'react'
 
 export default class Start extends React.Component {
+    componentDidMount(){
+        var myLatlng = new sogou.maps.LatLng(39.9952,116.3269);
+        var map = new sogou.maps.Map(this.mapDom, {
+            zoom: 50,
+            center: myLatlng,
+            hdMap:true,
+            mapTypeId: sogou.maps.MapTypeId.EDUSHIMAP,
+            mapControl: false
+        });
+        var labelOne = new sogou.maps.Label({
+            position: myLatlng,
+            map: map,
+            title: '清华大学'
+        });
+    }
     render() {
-        const {style, params: {name}} = this.props;
         return (
-            <div>
-
+            <div className="start-container">
+                <div ref={(c)=>this.mapDom = c} className="map_canvas"></div>
+                <a href="#/map" className="btn">Go!</a>
             </div>
         )
     }
