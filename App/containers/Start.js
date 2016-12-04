fb971d95ab105c45ae80ba380181ee18ea19f59f
tjz <415800467@qq.com>
2016-12-04 21:34:37 +0800

    声音

--- App/containers/Start.js
+++ App/containers/Start.js
@@ -19,13 +19,21 @@ export default class Start extends React.Component {
     componentWillUnmount(){
 
     }
+    go=()=>{
+        //let dong = document.getElementById('Dong');
+        this.dong.play();
+        setTimeout(function(){
+            location.href="/chat/";
+        }, 500);
+    }
     render() {
         return (
             <div className="start-container">
                 <div ref={(c)=>this.mapDom = c} className="map_canvas"></div>
                 <div className="btn-con">
-                    <a href="/map" onClick={this.go} className="btn">Go!</a>
+                    <a href="javascript:;" onClick={this.go} className="btn">Go!</a>
                 </div>
+                <audio style={{display: 'none'}} src="/App/audio/dongdong.mp3" ref={(c)=>this.dong=c} />
             </div>
         )
     }
