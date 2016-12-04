b9c73da855a4724cc5161e7c53ca39465abd1598
xiongsheng <huoyanwuzhe629@163.com>
2016-12-04 16:50:31 +0800

    更改样式

--- App/containers/Chat.js
+++ App/containers/Chat.js
@@ -1,14 +1,59 @@
 import React from 'react'
 
 export default class Chat extends React.Component {
+    state= {
+        display1: 'none',
+        display2: 'none',
+        display3: 'none',
+        display4: 'none'
+    }
+    componentDidMount() {
+        window.setTimeout(()=>{
+            this.appear(1);
+        }, 3000);
+        window.setTimeout(()=>{
+            this.appear(2);
+        }, 5000);
+        window.setTimeout(()=>{
+            this.appear(3);
+        }, 7000);
+        window.setTimeout(()=>{
+            this.appear(4);
+        }, 9000);
+    }
+    appear(type) {
+        switch(type) {
+            case 1 :
+                this.setState({
+                    display1: 'flex'
+                });
+                break;
+            case 2 :
+                this.setState({
+                    display2: 'flex'
+                });
+                break;
+            case 3 :
+                this.setState({
+                    display3: 'flex'
+                });
+                break;
+            case 4 :
+                this.setState({
+                    display4: 'flex'
+                });
+                break;
+        }
+    }
     render() {
-        const {style, params: {name}} = this.props;
+        const {style, params: {name}} = this.props,
+            {display1,display2, display3, display4} = this.state;
         return (
             <div className="chat-container">
-                <div className="chat-time">
+                <div className="chat-time" style={{display: display1}}>
                     <span>2016-11-17 18:26</span>
                 </div>
-                <div className="chat-content-right">
+                <div className="chat-content-right" style={{display: display1}}>
                     <div className="chat-msg">
                         <span>我想换一个讲解</span>
                     </div>
@@ -16,16 +61,15 @@ export default class Chat extends React.Component {
                         <img src="/App/img/group.png" />
                     </div>
                 </div>
-                <div className="chat-content-left">
+                <div className="chat-content-left" style={{display: display2}}>
                     <div className="chat-img">
                         <img src="/App/img/group.png" />
                     </div>
                     <div className="chat-msg">
                         <span>好的，已为您切换到高晓松的讲解。</span><br/>
-                        <a href="https://www.sogou.com">高晓松的讲解</a>
                     </div>
                 </div>
-                <div className="chat-content-right">
+                <div className="chat-content-right" style={{display: display3}}>
                     <div className="chat-msg">
                         <span>附近有啥吃午饭的地方么？</span>
                     </div>
@@ -33,13 +77,13 @@ export default class Chat extends React.Component {
                         <img src="/App/img/group.png" />
                     </div>
                 </div>
-                <div className="chat-content-left">
+                <div className="chat-content-left" style={{display: display4}}>
                     <div className="chat-img">
                         <img src="/App/img/group.png" />
                     </div>
                     <div className="chat-msg">
                         <span>你现在是不是该吃午饭了啊?</span><br/>
-                        <a href="https://www.sogou.com">这是适合吃饭的地方</a>
+                        <a href="http://www.sogou.com">附近美食</a>
                     </div>
                 </div>
 
